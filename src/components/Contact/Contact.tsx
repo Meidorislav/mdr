6e7cababb6af0f9523cbf8b2f99f576f7c9d2d2e
Vladislav Bakin <meidorislav@gmail.com>
2026-05-30 17:26:20 +0500

    feat: css styles for Contact components

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,15 +1,33 @@
+import styles from './Contact.module.css';
+
 const Contact = () => {
   return (
     <>
       <div>Initializing secure connection...</div>
       <div>[ OK ] GPG Key verified.</div>
       <div>[ OK ] Handshake complete.</div>
+
       <br />
-      <div><span style={{ color: 'var(--accent-color)' }}>Email:</span> meidorislav@gmail.com</div>
-      <div><span style={{ color: 'var(--accent-color)' }}>Telegram:</span> @meidorislav</div>
-      <div><span style={{ color: 'var(--accent-color)' }}>GitHub:</span> https://github.com/Meidorislav</div>
-      <div><span style={{ color: 'var(--accent-color)' }}>LinkedIn:</span> https://linkedin.com/in/meidorislav</div>
-      <div><span style={{ color: 'var(--accent-color)' }}>Location:</span> Perm, Russia</div>
+
+      <div>
+        <span className={styles.label}>Email:</span> meidorislav@gmail.com
+      </div>
+
+      <div>
+        <span className={styles.label}>Telegram:</span> @meidorislav
+      </div>
+
+      <div>
+        <span className={styles.label}>GitHub:</span> https://github.com/Meidorislav
+      </div>
+
+      <div>
+        <span className={styles.label}>LinkedIn:</span> https://linkedin.com/in/meidorislav
+      </div>
+
+      <div>
+        <span className={styles.label}>Location:</span> Perm, Russia
+      </div>
     </>
   );
 };
